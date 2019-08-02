c8c2c4213a30efe8815689854491c6826d5a7cd7
houkms <t-hochen@microsoft.com>
2019-08-02 12:33:27 +0800

    replace bash with sh logic

--- list_resources.sh
+++ list_resources.sh
@@ -1,19 +1,22 @@
-#!/bin/bash
+#!/bin/sh
 specs_dir="./specs"
 
-function findResourceDir(){
-    for file in `ls $1`
-    do
-    	work_dir=$1"/"$file
-        if [ -d $work_dir ]
-        then
-        	if [ -n "$(git status -s $work_dir)" ] && [ -f $work_dir"/api.yaml" ] \
-        		&& ([ -f $work_dir"/ansible.yaml" ] || [ -f $work_dir"/terraform.yaml" ]); then
-				echo ${work_dir:8}		# remove the directory prefix './specs/'
+findResourceDir(){
+	for file in `ls $1`
+	do
+		work_dir=$1"/"$file
+		if [ -d $work_dir ]
+		then
+			if [ -n "$(git status -s $work_dir)" ] && [ -f $work_dir"/api.yaml" ] \
+				&& ([ -f $work_dir"/ansible.yaml" ] || [ -f $work_dir"/terraform.yaml" ]);
+			then
+				# remove the directory prefix './specs/'
+				expr substr "$work_dir" 9 "${#work_dir}"
 			fi
-            findResourceDir $work_dir	# find resource directory recursively
-        fi
-    done
+			# find resource directory recursively
+			findResourceDir $work_dir
+		fi
+	done
 }
 
 findResourceDir $specs_dir
