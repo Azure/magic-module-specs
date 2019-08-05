ba9aa2c122c85be5f4d6cfc181e00ac144faf1c4
houkms <t-hochen@microsoft.com>
2019-08-05 20:43:15 +0800

    add listing resources script

--- list_resources.sh
+++ list_resources.sh
@@ -4,7 +4,7 @@ cd `dirname $0`
 for file in `ls ./specs`; do
 	work_dir=./specs/$file
 	if [ -d $work_dir ]; then
-		if [ -n "$(git status -s $work_dir)" ];
+		if [ -n "$(git diff --name-only $target_branch $source_branch -- $work_dir)" ];
 		then
 			# remove the directory prefix './specs/'
 			expr substr "$work_dir" 9 "${#work_dir}"
