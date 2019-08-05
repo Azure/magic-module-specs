7519caa3303ac5a5030d57029998d5302facedf7
houkms <t-hochen@microsoft.com>
2019-08-05 15:45:38 +0800

    code refined

--- list_resources.sh
+++ list_resources.sh
@@ -1,23 +1,13 @@
 #!/bin/sh
 
-findResourceDir(){
-	for file in `ls $1`
-	do
-		work_dir=$1"/"$file
-		if [ -d $work_dir ]
-		then
-			if [ -n "$(git status -s $work_dir)" ] && [ -f $work_dir"/api.yaml" ] \
-				&& ([ -f $work_dir"/ansible.yaml" ] || [ -f $work_dir"/terraform.yaml" ]);
-			then
-				# remove the directory prefix './specs'
-				expr substr "$work_dir" 9 "${#work_dir}"
-			fi
-			# find resource directory recursively
-			findResourceDir $work_dir
-		fi
-	done
-}
-# go to magic-module-specs directory
 cd `dirname $0`
-specs_dir='./specs'
-findResourceDir $specs_dir
+for file in `ls ./specs`; do
+	work_dir=./specs/$file
+	if [ -d $work_dir ]; then
+		if [ -n "$(git status -s $work_dir)" ];
+		then
+			# remove the directory prefix './specs/'
+			expr substr "$work_dir" 9 "${#work_dir}"
+		fi
+	fi
+done
