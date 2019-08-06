44a5e999fa351974c275e4c6ba26fc8b6be2a7ae
houkms <t-hochen@microsoft.com>
2019-08-06 13:36:28 +0800

    add fallback in script

--- list_resources.sh
+++ list_resources.sh
@@ -1,11 +1,26 @@
 #!/bin/sh
 
-cd `dirname $0`
-for file in `ls ./specs`; do
+cd $(dirname $0)
+
+# If Running Locally
+source_branch=$1
+target_branch=master
+if [ -z "$source_branch" ]; then
+	current_branch=$(git branch | grep '*')
+	source_branch="$(expr substr "$current_branch" 3 "${#current_branch}")"
+fi
+
+# If Running in Pipeline
+if [ -z "$(git branch -a | grep -w $source_branch)" ] || [ -z "$(git branch -a | grep -w $target_branch)" ]; then
+	source_branch=remotes/origin/$source_branch
+	target_branch=remotes/origin/$target_branch
+fi
+
+# List Resource Directories
+for file in $(ls ./specs); do
 	work_dir=./specs/$file
 	if [ -d $work_dir ]; then
-		if [ -n "$(git diff --name-only remotes/origin/master remotes/origin/$source_branch -- $work_dir)" ];
-		then
+		if [ -n "$(git diff --name-only $target_branch $source_branch -- $work_dir)" ]; then
 			# remove the directory prefix './specs/'
 			expr substr "$work_dir" 9 "${#work_dir}"
 		fi
